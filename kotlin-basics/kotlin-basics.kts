44f9809109fd9f9461c75590c2a11e1af7ed00a6
ayau8 <alvinyau@Alvins-MacBook-Pro.local>
2025-06-01 16:00:12 +0900

    practice: create user class

---
+++ kotlin-basics/kotlin-basics.kts
@@ -0,0 +1,26 @@
+// User class with properties and a method to print user details
+
+open class User (val name: String, val age: Int) {
+    open fun printUserDetails(): String {
+        return "I am $name and $age years old"
+    }
+}
+
+val firstUser = User(name = "Alvin", age = 30)
+println(firstUser.printUserDetails())
+
+// Inheritance User Class. Include Null Safety for ID
+class UserWithId (name: String, age: Int, val id: String?) : User(name, age) {
+    override fun printUserDetails(): String {
+        return "I am $name and $age years old. My ID is $id"
+    }
+}
+
+val secondUser = UserWithId(name = "Ken", age = 31, id = "1")
+println(secondUser.printUserDetails())
+
+val thirdUser = UserWithId(name = "Kento", age = 32, id = null)
+println(thirdUser.printUserDetails())
+
+
+
