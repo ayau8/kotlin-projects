4f2e57d0002a3bfd5d75046d1a30aeaed1674734
ayau8 <alvinyau@Alvins-MacBook-Pro.local>
2025-06-05 23:03:56 +0900

    hello world rest API with Spring Boot

---
+++ hello-world/src/main/java/com/codewithAY/hello_world/HomeController.java
@@ -0,0 +1,12 @@
+package com.codewithAY.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!\n";
+    }
+}
