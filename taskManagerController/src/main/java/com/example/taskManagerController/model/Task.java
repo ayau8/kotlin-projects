85edd90eca4720df5499de0abff8edb712cae377
ayau8 <alvinyau@Alvins-MacBook-Pro.local>
2025-06-07 18:35:30 +0900

    feat: task manager rest API

---
+++ taskManagerController/src/main/java/com/example/taskManagerController/model/Task.java
@@ -0,0 +1,58 @@
+package com.example.taskManagerController.model;
+import java.time.LocalDate;
+
+public class Task {
+    private Long id;
+    private String title;
+    private LocalDate date;
+    private String description;
+    private Boolean isCompleted;
+
+    public Task() {}
+
+    public Task(Long id, String title, LocalDate date, String description, Boolean isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.description = description;
+        this.isCompleted = isCompleted;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(Boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+}
