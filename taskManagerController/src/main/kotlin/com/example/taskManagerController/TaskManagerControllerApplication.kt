1261494698a0b7df2b621328ae36e2823aa6596e
ayau8 <alvinyau@Alvins-MacBook-Pro.local>
2025-06-23 00:57:40 +0900

    fix: update file directory

--- taskManagerController/src/main/kotlin/com/example/taskManagerController/TaskManagerControllerApplication.kt
+++ taskManagerController/src/main/kotlin/com/example/taskManagerController/TaskManagerControllerApplication.kt
@@ -1,4 +1,4 @@
-package com.example.taskManagerController.service;
+package com.example.taskManagerController;
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
