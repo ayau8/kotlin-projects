855815f30bfedbdb58614098d3fe796401d19959
ayau8 <alvinyau@Alvins-MacBook-Pro.local>
2025-07-04 14:30:02 +0900

    feat: add docker-compose for mysql

--- src/main/kotlin/com/example/taskManagerController/TaskManagerControllerApplication.kt
+++ src/main/kotlin/com/example/taskManagerController/TaskManagerControllerApplication.kt
@@ -2,10 +2,13 @@ package com.example.taskManagerController
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TaskManagerControllerApplication
+@EntityScan(basePackages = ["com.example.taskManagerController.entity"])
+class TaskManagerApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(TaskManagerControllerApplication::class.java, *args)
+    runApplication<TaskManagerApplication>(*args)
 }
